cf3efbc8b2186fb7b0310ef3ab1f7b3eb4d6ceb4
Christine Ibaraki <christineibaraki@Christines-MacBook-Pro.local>
2018-11-01 12:44:05 -0400

    add io utility

---
+++ main/utility/io_utility.py
@@ -0,0 +1,22 @@
+import json
+import os
+from os import path
+from main import MAIN_ROOT_DIR, DEMO_ROOT_DIR, TEST_ROOT_DIR, ROOT_DIR
+
+
+def read_dictionary_from_file(filepath):
+    with open(filepath, encoding="utf-8") as f:
+        lines = f.readlines()
+        return json.loads("".join(lines))
+
+def standardize_path(path_string):
+    path_string = path_string.strip()
+    path_string = path_string.replace("\\", "/")
+    return path_string
+
+def __get_relative_path(relative_to, relative_path):
+    relative_path = standardize_path(relative_path)
+    return standardize_path(os.path.join(relative_to, relative_path))
+
+def get_project_path(relative_path):
+    return __get_relative_path(ROOT_DIR, relative_path)
