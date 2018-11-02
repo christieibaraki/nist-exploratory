43de35ceb578eb730534628e7949c33e25e613e9
Christine Ibaraki <christineibaraki@Christines-MacBook-Pro.local>
2018-11-02 10:08:36 -0400

    add fields to be generated, call to super constructor

--- main/generation/user/user_data_generator.py
+++ main/generation/user/user_data_generator.py
@@ -3,5 +3,10 @@ from main.generation.abstract_data_generator import AbstractDataGenerator
 
 class UserDataGenerator(AbstractDataGenerator):
 
+    FIELDS = ["FIRST_NAME", "LAST_NAME", "ACCOUNT_NUMBER", "USERNAME", "DEPARTMENT", "HIRE_DATE", "ADDRESS", "ROLE"]
+
+    def __init__(self):
+        super().__init__(UserDataGenerator.FIELDS)
+
     def generate_data(self):
         pass
